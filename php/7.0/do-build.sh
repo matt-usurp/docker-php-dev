15433a44c4c5bd93bddc8d7aef297aa5a8332ca9
Matthew Usurp <matt.usurp@gmail.com>
2018-12-07 13:51:50 +0000

    Prepared all versions for docker image availability 🍤

--- php/7.0/do-build.sh
+++ php/7.0/do-build.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/../util-colours.sh
 IMAGE="musurp/php"
 IMAGE_DEV="musurp/php-dev"
 
-VERSION_FULL="7.0.32"
+VERSION_FULL="7.0.33"
 VERSION_MINOR="7.0"
 
 green "Targeting php@${VERSION_FULL}"
