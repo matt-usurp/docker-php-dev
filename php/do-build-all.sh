a956f1f726f0e2d05a92f7a2f05bf33fa5f4e772
Matthew Usurp <matt.usurp@gmail.com>
2019-02-01 20:55:26 +0000

    Reversed execution to build latest images first.

--- php/do-build-all.sh
+++ php/do-build-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(cd php/7.0 && bash do-build.sh)
-(cd php/7.1 && bash do-build.sh)
-(cd php/7.2 && bash do-build.sh)
 (cd php/7.3 && bash do-build.sh)
+(cd php/7.2 && bash do-build.sh)
+(cd php/7.1 && bash do-build.sh)
+(cd php/7.0 && bash do-build.sh)
--- php/do-push-all.sh
+++ php/do-push-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(cd php/7.0 && bash do-push.sh)
-(cd php/7.1 && bash do-push.sh)
-(cd php/7.2 && bash do-push.sh)
 (cd php/7.3 && bash do-push.sh)
+(cd php/7.2 && bash do-push.sh)
+(cd php/7.1 && bash do-push.sh)
+(cd php/7.0 && bash do-push.sh)
