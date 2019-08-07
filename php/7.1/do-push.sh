9375d94c4c69632385ce936e9b27b822e23faee7
Matthew Usurp <matt.usurp@gmail.com>
2019-08-07 13:02:16 +0100

    Version bumping to latest `7.3.7`, `7.2.20` and `7.1.31`

--- php/7.1/do-push.sh
+++ php/7.1/do-push.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/../util-colours.sh
 IMAGE="musurp/php"
 IMAGE_DEV="musurp/php-dev"
 
-VERSION_FULL="7.1.30"
+VERSION_FULL="7.1.13"
 VERSION_MINOR="7.1"
 
 green "Pushing php@${VERSION_FULL}"
